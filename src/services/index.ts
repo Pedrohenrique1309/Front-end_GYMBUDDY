fddc70d133b7293fbf4eb74e90e641e1bd1b1fae
LUCAS RODRIGUES NOBRE SILVA <24122499@dsn127m1242314.cfp127aluno.local>
2025-10-09 09:15:29 -0300

    login pop up corrigido

--- src/services/index.ts
+++
@@ -1,9 +0,0 @@
-// Exportações centralizadas de todos os serviços
-export * from './api';
-export * from './userService';
-export * from './publicationService';
-
-// Exportações default
-export { default as api } from './api';
-export { default as userService } from './userService';
-export { default as publicationService } from './publicationService';
